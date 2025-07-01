87f82099956057dc0ae29746e96f1c37bff27942
mangrusdi <132583394+mangrusdi@users.noreply.github.com>
2025-07-01 18:26:30 +0700

    Delete koneksi.php

--- koneksi.php
+++
@@ -1,9 +0,0 @@
-<?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$db = "infaq";
-
-$conn = mysqli_connect($host, $user, $pass, $db) or die("Koneksi gagal");
-mysqli_set_charset($conn, "utf8");
-?>
